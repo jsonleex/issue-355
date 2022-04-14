9b9815d71b4f14aac7ae028170c2cb40d8e894fe
shuail <shuail@moqi.ai>
2022-04-14 15:42:16 +0800

    chore: reproduce issue 355

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,20 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import Components from 'unplugin-vue-components/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      dirs: ['src/components'],
+      dts: 'src/components.d.ts',
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
